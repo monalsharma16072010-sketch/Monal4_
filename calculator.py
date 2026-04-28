826001ab7ce0678c60c221b8d18d746992e45257
monalsharma16072010-sketch <monalsharma16072010@gmail.com>
2026-04-28 20:55:30 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,23 @@
+# Simple Calculator (Integer Only)
+
+num1 = int(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = int(input("Enter second number: "))
+
+if operator == "+":
+    print("Result =", num1 + num2)
+
+elif operator == "-":
+    print("Result =", num1 - num2)
+
+elif operator == "*":
+    print("Result =", num1 * num2)
+
+elif operator == "/":
+    if num2 != 0:
+        print("Result =", num1 // num2)  # integer division
+    else:
+        print("Error: Division by zero")
+
+else:
+    print("Invalid operator")
